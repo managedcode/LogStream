4644949ce4f7740677be4185ed62013cc8b61ab8
Konstantin Semenenko <mail@ksemenenko.com>
2023-04-06 13:12:15 +0200

    init

---
+++ ManagedCode.LogStream.Tests/Tests.cs
@@ -0,0 +1,13 @@
+using ManagedCode.LogStream;
+using Xunit;
+
+namespace ManagedCode.LogStream.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void TestDetectorTest()
+    {
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LogStream
+
+
