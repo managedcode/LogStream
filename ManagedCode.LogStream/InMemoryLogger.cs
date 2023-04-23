d4f7391768b71ec0c9b8a5c5db515b1927ed7e92
Elena Bespamyatnova <elena.bespamyatnova.1997@gmail.com>
2023-04-23 15:22:20 +0300

    Added InMemoryLogger, LogModel and LogProvider classes

---
+++ ManagedCode.LogStream/InMemoryLogger.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace ManagedCode.LogStream
+{
+    public class InMemoryLogger : ILogger
+    {
+        
+        private readonly string _nameCategory;        
+        private static List<LogModel> logs = new List<LogModel>();
+        private IExternalScopeProvider _scopeProvider;
+        public static LogLevel minLogLevel { get; set; }
+
+
+        public InMemoryLogger(string nameCategory)
+        {
+            _nameCategory = nameCategory;
+            _scopeProvider = new LoggerExternalScopeProvider();
+        }
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return _scopeProvider?.Push(state);
+            
+        }
+
+        public static int checkEnableLevel(LogLevel level)
+        {
+            if ((int)minLogLevel > (int)level)
+            {
+                return -1;
+            }
+            return (int)level;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {           
+            if (checkEnableLevel(logLevel) < 0)
+            {
+                throw new InvalidOperationException("Invalid logging level!");
+            }
+            return true; 
+            
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            
+            if (IsEnabled(logLevel))
+            {
+                LogModel log = new LogModel()
+                {
+                    level = logLevel,
+                    eventId = eventId.Id,                    
+                    state = state,
+                    dateTime = DateTime.Now,
+                    error = exception,
+                    message = formatter(state, exception)
+                };
+
+                logs.Add(log);
+            }
+        }
+
+        public static IEnumerable<LogModel> GetLogTrace() 
+        {
+           return logs.Where(l => l.level.Equals(LogLevel.Trace));  
+        }
+        public static IEnumerable<LogModel> GetLogDebug()
+        {
+            return logs.Where(l => l.level.Equals(LogLevel.Debug));
+        }
+        public static IEnumerable<LogModel> GetLogInformation()
+        {
+            return logs.Where(l => l.level.Equals(LogLevel.Information));
+        }
+        public static IEnumerable<LogModel> GetLogWarning()
+        {
+            return logs.Where(l => l.level.Equals(LogLevel.Warning));
+        }
+        public static IEnumerable<LogModel> GetLogError()
+        {
+            return logs.Where(l => l.level.Equals(LogLevel.Error));
+        }
+        public static IEnumerable<LogModel> GetLogCritical()
+        {
+            return logs.Where(l => l.level.Equals(LogLevel.Critical));
+        }
+        public static IEnumerable<LogModel> GetAllLogs()
+        {
+            return logs.FindAll(l => l.level != LogLevel.None);
+        }
+    }
+}
---
+++ ManagedCode.LogStream/LogProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedCode.LogStream
+{
+    public class LogProvider:ILoggerProvider
+    {
+        
+        public ILogger CreateLogger(string nameCategory)
+        {
+            return new InMemoryLogger(nameCategory);
+            
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+}
